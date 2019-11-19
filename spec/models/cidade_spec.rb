31c9a11a04f8b235f12f2df9e91bfc9f83eca4e5
FelipeEckerEcker <felipeecker11@gmail.com>
2019-11-19 01:12:15 -0300

    comitando coverage e spec modificados

--- spec/models/cidade_spec.rb
+++ spec/models/cidade_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe Cidade, type: :model do
     it { should validate_presence_of(:cep) }
     it { should validate_presence_of(:bairro) }
   end
+
+  context 'ao informar estado' do
+    let(:cidade) { FactoryBot.create(:cidade) }
+
+    context 'duas vezes no mesmo estado' do
+      let(:cidade_repetida) { FactoryBot.build(:cidade,
+                                                  estado: cidade.estado) }
+
+      it 'e em nomes diferentes, deve permitir' do
+        cidade_repetida.nome = "Dois Vizinhos"
+        expect(cidade_repetida).to be_valid
+      end
+
+      context 'em dois ceps distintos no mesmo estado' do
+        let(:cidade2) { FactoryBot.build(:cidade) }
+        it 'deve permitir' do
+          expect(cidade2).to be_valid
+        end
+      end
+      end
+  end
 end
